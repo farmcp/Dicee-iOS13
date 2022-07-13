9a6567ef389c597387d067181e2c8cdbb5d2f8d3
Christopher Farm <christopherfarm@Christophers-MacBook-Pro.local>
2022-07-13 16:30:44 -0700

    make changes to roll dice randomly

--- DiceRoll/ViewController.swift
+++ DiceRoll/ViewController.swift
@@ -1,20 +1,26 @@
 //
 //  ViewController.swift
-//  Dicee-iOS13
-//
-//  Created by Angela Yu on 11/06/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
-//
+//  Edited by Christopher Farm
 
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    let diceImages = [UIImage (imageLiteralResourceName: "DiceOne"),UIImage (imageLiteralResourceName: "DiceTwo"),UIImage (imageLiteralResourceName: "DiceThree"),UIImage (imageLiteralResourceName: "DiceFour"),UIImage (imageLiteralResourceName: "DiceFive"),UIImage (imageLiteralResourceName: "DiceSix") ]
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let leftDiceNumber = Int.random(in: 0...5) //randomly generate an index
+        let rightDiceNumber = Int.random(in: 0...5) //randomly generate an index
+        
+        diceImageView1.image = diceImages[leftDiceNumber]
+        diceImageView2.image = diceImages[rightDiceNumber]
+        
+        
     }
-
+    
 
 }
 
